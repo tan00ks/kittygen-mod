ac98fae0a9631f9cf78dc70d1dd7323f33d76ede
sedgestripe <110428732+sedgestripe@users.noreply.github.com>
2023-10-07 21:48:09 -0500

    queen's app fix, infinite loading bug attempt fix

--- scripts/utility.py
+++ scripts/utility.py
@@ -1019,34 +1019,6 @@ def event_text_adjust(Cat,
 
     return adjust_text
 
-def event_text_adjust2(Cat, text, cat, other_cat=None,
-                      other_clan_name=None,
-                      new_cat=None,
-                      clan=None,
-                      murder_reveal=None,
-                      victim=None):
-    if "r_w" in text and game.clan:
-        random_warrior = Cat.fetch_cat(choice(game.clan.clan_cats))
-        random_warrior2 = None
-        counter = 0
-        while random_warrior.status != "warrior" or len(random_warrior.mate) > 0 or random_warrior.ID == cat.ID:
-            random_warrior = Cat.fetch_cat(choice(game.clan.clan_cats))
-            counter += 1
-            if counter == 30:
-                break
-        if "r_a" in text:
-            random_warrior2 = Cat.fetch_cat(choice(game.clan.clan_cats))
-            counter = 0
-            while random_warrior2.ID == cat.ID or random_warrior2.ID == random_warrior.ID or random_warrior2.status != "warrior" or len(random_warrior2.mate) > 0 or random_warrior2.is_related(random_warrior, False):
-                random_warrior2 = Cat.fetch_cat(choice(game.clan.clan_cats))
-                counter += 1
-                if counter == 30:
-                    break
-            text = text.replace("r_a", str(random_warrior2.name))
-        text = text.replace("r_w", str(random_warrior.name))
-    return text
-
-
 def leader_ceremony_text_adjust(Cat,
                                 text,
                                 leader,
