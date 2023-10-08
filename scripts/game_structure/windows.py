120f4c06054019c2481548d7097010b1b3cc3695
sedgestripe <110428732+sedgestripe@users.noreply.github.com>
2023-10-07 20:21:15 -0500

    Update windows.py

--- scripts/game_structure/windows.py
+++ scripts/game_structure/windows.py
@@ -922,33 +922,9 @@ class ChangelogPopup(UIWindow):
             manager=MANAGER)
 
         dynamic_changelog = False
-        if get_version_info().is_dev and get_version_info().is_source_build and get_version_info().git_installed:
-            file_cont = subprocess.check_output(
-                ["git", "log", r"--pretty=format:%H|||%cd|||%b|||%s", "-15", "--no-decorate", "--merges", "--grep=Merge pull request", "--date=short"]).decode("utf-8")
-            dynamic_changelog = True
-        else:
-            with open("changelog.txt", "r") as read_file:
-                file_cont = read_file.read()
         
-        if get_version_info().is_dev and not get_version_info().is_source_build:
-            dynamic_changelog = True
-
-        if dynamic_changelog:
-            commits = file_cont.splitlines()
-            file_cont = ""
-            for line in commits:
-                info = line.split("|||")
-                                
-                # Get PR number so we can link the PR
-                pr_number = re_search(r"Merge pull request #([0-9]*?) ", info[3])
-                if pr_number:
-                    
-                    # For some reason, multi-line links on pygame_gui's text boxes don't work very well. 
-                    # So, to work around that, just add a little "link" at the end
-                    info[2] += f" <a href='https://github.com/Thlumyn/clangen/pull/{pr_number.group(1)}'>(link)</a>"
-                
-                # Format: DATE- \n PR Title (link)
-                file_cont += f"<b>{info[1]}</b>\n- {info[2]}\n"
+        with open("changelog.txt", "r") as read_file:
+            file_cont = read_file.read()
 
         self.changelog_text = UITextBoxTweaked(
             file_cont,
