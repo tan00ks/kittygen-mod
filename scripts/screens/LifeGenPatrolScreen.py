1fd3fd1347ccf2b2c43f79c8a6728bb64e3c7b65
sedgestripe <110428732+sedgestripe@users.noreply.github.com>
2024-01-18 15:24:00 -0600

    add murder screen art

--- scripts/screens/LifeGenPatrolScreen.py
+++ scripts/screens/LifeGenPatrolScreen.py
@@ -662,6 +662,19 @@ class LifeGenPatrolScreen(Screens):
             previous page buttons.  """
         self.clear_cat_buttons()  # Clear all the cat buttons
 
+        if not game.clan.your_cat:
+            print("Are you playing a normal ClanGen save? Switch to a LifeGen save or create a new cat!")
+            print("Choosing random cat to play...")
+            game.clan.your_cat = Cat.all_cats[choice(game.clan.clan_cats)]
+            counter = 0
+            while game.clan.your_cat.dead or game.clan.your_cat.outside:
+                if counter == 25:
+                    break
+                game.clan.your_cat = Cat.all_cats[choice(game.clan.clan_cats)]
+                counter+=1
+                
+            print("Chose " + str(game.clan.your_cat.name))
+
         self.able_cats = []
         the_cat = game.clan.your_cat
         if "patrolled" not in game.switches:
