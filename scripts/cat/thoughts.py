f8945a8ed8b815131f801df657eacd6186dd0c86
sedgestripe <110428732+sedgestripe@users.noreply.github.com>
2024-01-28 16:09:19 -0600

    Update thoughts.py

--- scripts/cat/thoughts.py
+++ scripts/cat/thoughts.py
@@ -315,9 +315,12 @@ class Thoughts():
             with open(f"{base_path}{life_dir}{spec_dir}/general.json", 'r') as read_file:
                 GENTHOUGHTS = ujson.loads(read_file.read())
             SHUNNEDTHOUGHTS = []
-            if not main_cat.dead and not main_cat.outside and  main_cat.revealed != 0 and game.clan.age - 3 <= main_cat.revealed:
-                with open(f"{base_path}{life_dir}{spec_dir}/shunned.json", 'r') as read_file:
-                    SHUNNEDTHOUGHTS = ujson.loads(read_file.read())
+            try:
+                if not main_cat.dead and not main_cat.outside and  main_cat.revealed != 0 and game.clan.age - 3 <= main_cat.revealed:
+                    with open(f"{base_path}{life_dir}{spec_dir}/shunned.json", 'r') as read_file:
+                        SHUNNEDTHOUGHTS = ujson.loads(read_file.read())
+            except:
+                pass
             loaded_thoughts = THOUGHTS 
             loaded_thoughts += GENTHOUGHTS
             loaded_thoughts += SHUNNEDTHOUGHTS
