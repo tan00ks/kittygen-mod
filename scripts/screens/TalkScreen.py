852daa126f85b15b9d696ef37a79a6daf6feee79
sedgestripe <110428732+sedgestripe@users.noreply.github.com>
2024-01-28 16:39:13 -0600

    update

--- scripts/screens/TalkScreen.py
+++ scripts/screens/TalkScreen.py
@@ -687,13 +687,26 @@ class TalkScreen(Screens):
             game.clan.talks.clear()
 
         weights2 = []
-        weighted_tags = ["from_your_parent", "from_adopted_parent", "adopted_parent", "half sibling", "littermate", "siblings_mate", "cousin", "adopted_sibling", "parents_siblings", "from_mentor", "from_your_kit", "from_your_apprentice", "from_mate", "from_parent", "adopted_parent", "from_kit", "sibling", "from_adopted_kit"]
+        weighted_tags = ["you_pregnant", "they_pregnant", "from_mentor", "from_your_parent", "from_adopted_parent", "adopted_parent", "half sibling", "littermate", "siblings_mate", "cousin", "adopted_sibling", "parents_siblings", "from_mentor", "from_your_kit", "from_your_apprentice", "from_mate", "from_parent", "adopted_parent", "from_kit", "sibling", "from_adopted_kit"]
         for item in texts_list.values():
             tags = item["tags"] if "tags" in item else item[0]
             num_fam_mentor_tags = 1
             if any(i in weighted_tags for i in tags):
                 num_fam_mentor_tags+=3
             weights2.append(num_fam_mentor_tags)
+
+        if "debug_ensure_dialogue" in game.config and game.config["debug_ensure_dialogue"]:
+            if game.config["debug_ensure_dialogue"] in list(texts_list.keys()):
+                text_chosen_key = game.config["debug_ensure_dialogue"] 
+                text = texts_list[text_chosen_key]["intro"] if "intro" in texts_list[text_chosen_key] else texts_list[text_chosen_key][1]
+                new_text = self.get_adjusted_txt(text, cat)
+                if new_text:
+                    if "intro" in texts_list[text_chosen_key]:
+                        self.text_type = "choices"
+                        self.display_intro(cat, texts_list, text_chosen_key)
+                    return new_text
+            else:
+                print("something's wrong")
 
         while counter < max_retries:
             text_chosen_key = choices(list(texts_list.keys()), weights=weights2, k=1)[0]
