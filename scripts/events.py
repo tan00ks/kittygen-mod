384e16aa076b27a91956a364498c711606baa1c9
sedgestripe <110428732+sedgestripe@users.noreply.github.com>
2024-01-28 18:17:44 -0600

    Update events.py

--- scripts/events.py
+++ scripts/events.py
@@ -495,10 +495,11 @@ class Events:
             #code for achievement 23 + 24
             if not Cat.all_cats.get(cat).dead and not Cat.all_cats.get(cat).outside:
                 count_alive_cats += 1
-            if count_alive_cats == 1 and Cat.all_cats.get(cat).ID == you.ID:
-                achievements.add('23')
-            elif count_alive_cats >= 100:
-                achievements.add('24')
+
+        if count_alive_cats == 1 and not you.dead and not you.outside:
+            achievements.add('23')
+        elif count_alive_cats >= 100:
+            achievements.add('24')
 
         if you.joined_df:
             achievements.add("7")
