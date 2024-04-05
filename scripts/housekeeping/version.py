4d3922fb24f9233a48cab5dde31854e3719d583f
sedgestripe <110428732+sedgestripe@users.noreply.github.com>
2024-04-04 19:38:43 -0500

    changelog

--- scripts/housekeeping/version.py
+++ scripts/housekeeping/version.py
@@ -7,7 +7,7 @@ from configparser import ConfigParser
 from platformdirs import user_data_dir
 logger = logging.getLogger(__name__)
 
-VERSION_NAME = "v0.6.8"
+VERSION_NAME = "v0.7.0"
 # This is saved in the Clan save-file, and is used for save-file converstion.
 SAVE_VERSION_NUMBER = 3
 
