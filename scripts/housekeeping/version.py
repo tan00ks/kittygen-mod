16d718432d45acefb636baeca68bff53d59f46d3
sedgestripe <110428732+sedgestripe@users.noreply.github.com>
2024-05-05 18:47:24 -0500

    add new dialogue and changelog

--- scripts/housekeeping/version.py
+++ scripts/housekeeping/version.py
@@ -7,7 +7,7 @@ from configparser import ConfigParser
 from platformdirs import user_data_dir
 logger = logging.getLogger(__name__)
 
-VERSION_NAME = "v0.7.0"
+VERSION_NAME = "v0.7.1"
 # This is saved in the Clan save-file, and is used for save-file converstion.
 SAVE_VERSION_NUMBER = 3
 
